e0fd2f40a01535c8f02417fd20fb52b26546eaf0
Rodrigo Santa Eulalia <rodrigo.santaeulalia@gmail.com>
2021-07-20 18:51:28 -0300

    added action to retrieve all movies in repository

--- src/main/kotlin/com/example/smallcinema/domain/action/FetchMoviesAction.kt
+++ src/main/kotlin/com/example/smallcinema/domain/action/FetchMoviesAction.kt
@@ -1,3 +1,12 @@
 package com.example.smallcinema.domain.action
 
-class FetchMoviesAction()
+import com.example.smallcinema.domain.dataprovider.MovieDataProvider
+import com.example.smallcinema.domain.model.Movie
+import org.springframework.stereotype.Service
+
+@Service
+class FetchMoviesAction(private val movieDataProvider: MovieDataProvider) : SmallCinemaAction<List<Movie>> {
+    override fun execute(): List<Movie> {
+        return movieDataProvider.findAll()
+    }
+}
---
+++ src/test/kotlin/com/example/smallcinema/domain/action/FetchMoviesActionTest.kt
@@ -0,0 +1,92 @@
+package com.example.smallcinema.domain.action
+
+import com.example.smallcinema.domain.dataprovider.MovieDataProvider
+import com.example.smallcinema.domain.model.Movie
+import com.example.smallcinema.domain.model.Review
+import com.example.smallcinema.domain.model.ShowTime
+import com.example.smallcinema.domain.model.TimeSchedule
+import io.mockk.MockKAnnotations.init
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class FetchMoviesActionTest {
+
+    lateinit var fetchMoviesAction: FetchMoviesAction
+
+    @MockK
+    private lateinit var movieDataProvider: MovieDataProvider
+
+    @BeforeEach
+    fun setUp() {
+        init(this)
+        fetchMoviesAction = FetchMoviesAction(movieDataProvider)
+    }
+
+    companion object {
+        var movie1 = Movie(
+            null, "Furious 7", null, "tt2820852", LocalDate.now().minusDays(7),
+            LocalDate.now().plusDays(7), listOf(
+                ShowTime(
+                    null, DayOfWeek.FRIDAY, listOf(
+                        TimeSchedule(null, LocalDateTime.now().minusHours(3), 5.5F),
+                        TimeSchedule(null, LocalDateTime.now().minusHours(1), 5.5F)
+                    )
+                )
+            ), listOf()
+        )
+
+        var movie2 = Movie(
+            null, "2 Fast 2 Furious", null, "tt0322259", LocalDate.now().minusDays(7),
+            LocalDate.now().plusDays(7), listOf(
+                ShowTime(
+                    null, DayOfWeek.FRIDAY, listOf(TimeSchedule(null, LocalDateTime.now().minusHours(3), 5.5F))
+                ),
+                ShowTime(
+                    null,
+                    DayOfWeek.SATURDAY,
+                    listOf(TimeSchedule(null, LocalDateTime.now().minusHours(3), 5.5F))
+                )
+            ), listOf()
+        )
+
+        var movie3 = Movie(
+            null, "Fast Five", null, "tt1596343", LocalDate.now().minusDays(7),
+            LocalDate.now().plusDays(7), listOf(
+                ShowTime(
+                    null, DayOfWeek.FRIDAY, listOf(TimeSchedule(null, LocalDateTime.now().minusHours(3), 5.5F))
+                )
+            ), listOf(Review(null, "Test Customer", "Best movie ever", 5F))
+        )
+    }
+
+    @Test
+    fun `given a execute action on fetch movies when fetch all then movies list must be returned`() {
+        val moviesList = listOf(movie1, movie2)
+
+        every { movieDataProvider.findAll() } returns moviesList
+
+        val packageDetail = fetchMoviesAction.execute()
+
+        verify(exactly = 1) { movieDataProvider.findAll() }
+        assertEquals(packageDetail, moviesList)
+    }
+
+    @Test
+    fun `given a execute action on fetch movies when no movies exists on repository then an empty list must be returned`() {
+
+        every { movieDataProvider.findAll() } returns emptyList()
+
+        val packageDetail = fetchMoviesAction.execute()
+
+        verify(exactly = 1) { movieDataProvider.findAll() }
+        assertTrue(packageDetail.isEmpty())
+    }
+}
