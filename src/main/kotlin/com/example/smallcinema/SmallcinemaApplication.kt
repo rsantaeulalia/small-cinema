17922c80d2fbf317cb0f27bf1f4d8dcd06cd9990
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-07-19 23:41:26 -0300

    First commit, template from Spring initializer

---
+++ src/main/kotlin/com/example/smallcinema/SmallcinemaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.smallcinema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmallcinemaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmallcinemaApplication>(*args)
+}
