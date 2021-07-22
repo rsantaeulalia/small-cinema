56b306eada79c7af54ac54785f4ac7066c257aad
Rodrigo Santa Eulalia <rodrigo.santaeulalia@gmail.com>
2021-07-22 11:14:45 -0300

    minor fixes for omdbClient

--- src/main/kotlin/com/example/smallcinema/infra/dataprovider/http/OmdbClient.kt
+++ src/main/kotlin/com/example/smallcinema/infra/dataprovider/http/OmdbClient.kt
@@ -4,6 +4,8 @@ import com.example.smallcinema.domain.dataprovider.OmdbDataProvider
 import com.example.smallcinema.domain.exception.ImdbClientFailureException
 import com.example.smallcinema.domain.exception.MovieNotFoundOnOmdbException
 import com.example.smallcinema.infra.model.OmdbMovie
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import kong.unirest.Unirest
 import org.springframework.beans.factory.annotation.Value
@@ -19,6 +21,7 @@ class OmdbClient(
 ) : OmdbDataProvider {
 
     override fun getMovieDetailByImdbId(imdbId: String): OmdbMovie? {
+        configObjectMapper()
         return runCatching {
             val response = Unirest.get("$imdbUrl${location.replace("{apiKey}", apiKey).replace("{imdbID}", imdbId)}")
                 .header("Content-Type", "application/json")
@@ -33,5 +36,12 @@ class OmdbClient(
         }.onFailure { e ->
             throw ImdbClientFailureException()
         }.getOrDefault(null)
+    }
+
+    private fun configObjectMapper() {
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_VALUES, true)
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
 }
